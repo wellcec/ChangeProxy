9034726412673f84d25fcf3e473767ab9bd7ee0e
Wellington Felix <wellington.felix@squadra.com.br>
2019-03-13 12:50:39 -0300

    Primeiro commit

---
+++ CProxy/Main.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace CProxy
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ntfMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.cmsMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ntfMain
+            // 
+            this.ntfMain.ContextMenuStrip = this.cmsMain;
+            this.ntfMain.Text = "Proxy";
+            this.ntfMain.Visible = true;
+            this.ntfMain.DoubleClick += new System.EventHandler(this.ntfMain_DoubleClick);
+            // 
+            // cmsMain
+            // 
+            this.cmsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.cmsMain.Name = "cmsMain";
+            this.cmsMain.Size = new System.Drawing.Size(181, 48);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(43, 12);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(98, 37);
+            this.btnFechar.TabIndex = 1;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 61);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnFechar);
+            this.Enabled = false;
+            this.MaximumSize = new System.Drawing.Size(207, 100);
+            this.MinimumSize = new System.Drawing.Size(207, 100);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.cmsMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon ntfMain;
+        private System.Windows.Forms.ContextMenuStrip cmsMain;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ CProxy/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace CProxy
+{
+    public partial class Main : Form
+    {
+        private const string SUBKEY = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+        public Main()
+        {
+            InitializeComponent();
+            GetStatusProxy();
+            Application.Run();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+            Application.Exit();
+        }
+
+        void GetStatusProxy()
+        {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey(SUBKEY, true);
+            var p_ = registry.GetValue("ProxyEnable");
+            if (Convert.ToInt32(p_) != 1)
+            {
+                registry.SetValue("ProxyEnable", 1);
+                ntfMain.Icon = new Icon("green.ico");
+                return;
+            }
+            registry.SetValue("ProxyEnable", 0);
+            ntfMain.Icon = new Icon("red.ico");
+        }
+
+        private void ntfMain_DoubleClick(object sender, EventArgs e)
+        {
+            GetStatusProxy();
+        }
+    }
+}
